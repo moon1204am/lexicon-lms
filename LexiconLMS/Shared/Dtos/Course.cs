aec6941aace44747a70b988f51d5471ae89d3651
moon1204am <may_aj@outlook.com>
2023-12-28 14:29:28 +0100

    removed api, moved entities

---
+++ LexiconLMS/Shared/Dtos/Course.cs
@@ -0,0 +1,22 @@
+﻿namespace LexiconLMS.Shared.Entities
+{
+    public class Course
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        //Foreign Keys
+        public Guid UserId { get; set; }
+        public Guid ModuleId { get; set; }
+
+        //Navigation Property
+        public ICollection<User> Users { get; set; } = new List<User>();
+        public ICollection<Module> Modules { get; set; } = new List<Module>();
+
+    }
+
+
+}
---
+++ LexiconLMS/Shared/Dtos/User.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiconLMS.Shared.Entities
+{
+    public class User
+    {
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        //Foreign Keys
+        public Guid CourseId { get; set; }
+
+        //Navigation Propery
+        public Course Course { get; set; } = new Course();
+    }
+}
