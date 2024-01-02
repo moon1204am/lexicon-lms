cd05a901d1e4364bb1a06fff0fd5966c42c8b810
Rabben82 <89846052+Rabben82@users.noreply.github.com>
2024-01-02 14:10:28 +0100

    Fixed Null reference Error in Courses.razor

--- LexiconLMS/Client/Pages/Courses.razor.cs
+++ LexiconLMS/Client/Pages/Courses.razor.cs
@@ -6,13 +6,13 @@ namespace LexiconLMS.Client.Pages
 {
     public partial class Courses
     {
-        public List<CourseDto>? courses = default!;
+        public List<CourseDto>? CourseList = default!;
 
         [Inject] public ILmsDataService LmsDataService { get; set; } = default!;
 
         protected override async Task OnInitializedAsync()
         {
-            courses = (await LmsDataService.GetAsync<List<CourseDto>>("api/courses"))!; //ToDo: fix nullability
+            CourseList = (await LmsDataService.GetAsync<List<CourseDto>>("api/courses"))!; //ToDo: fix nullability
         }
     }
 }
