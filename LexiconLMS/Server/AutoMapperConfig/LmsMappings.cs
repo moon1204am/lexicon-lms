72823206a05e8e441fa3fc849821bd553dd25b56
Rabben82 <89846052+Rabben82@users.noreply.github.com>
2024-01-03 13:24:31 +0100

    add courses in developement

--- LexiconLMS/Server/AutoMapperConfig/LmsMappings.cs
+++ LexiconLMS/Server/AutoMapperConfig/LmsMappings.cs
@@ -8,11 +8,13 @@ namespace LexiconLMS.Server.AutoMapperConfig
     {
         public LmsMappings() 
         { 
+            //ToDo: Evaluate and remove unused DTOs and mappings.
             CreateMap<Course, CourseDto>().ReverseMap();
             CreateMap<ApplicationUser, UserDto>().ReverseMap();
             CreateMap<Module, ModuleDto>().ReverseMap();
             CreateMap<Activity, ActivityDto>().ReverseMap();
             CreateMap<ActivityType, ActivityTypeDto>().ReverseMap();
+            CreateMap<Course, CourseAddDto>().ReverseMap();
         }
     }
 }
