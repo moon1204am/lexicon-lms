b403d14e3b04792729f88ae9b8712c7c477622e0
Rabben82 <89846052+Rabben82@users.noreply.github.com>
2024-01-03 17:00:07 +0100

    Changed To Edit Form

--- LexiconLMS/Shared/Dtos/CourseAddDto.cs
+++ LexiconLMS/Shared/Dtos/CourseAddDto.cs
@@ -6,10 +6,10 @@ namespace LexiconLMS.Shared.Dtos
     public class CourseAddDto
     {
         [Required]
-        [StringLength(maximumLength: 30, MinimumLength = 2, ErrorMessage = "The {0} need to be between {1} and {2} characters long.")]
+        [StringLength(maximumLength: 30, MinimumLength = 2, ErrorMessage = "The {0} need to be between {2} and {1} characters long.")]
         public string Name { get; set; } = string.Empty;
         [Required]
-        [StringLength(maximumLength: 100, MinimumLength = 2, ErrorMessage = "The {0} need to be between {1} and {2} characters long.")]
+        [StringLength(maximumLength: 100, MinimumLength = 2, ErrorMessage = "The {0} need to be between {2} and {1} characters long.")]
         public string Description { get; set; } = string.Empty;
         [Required]
         public DateTime StartDate { get; set; }
