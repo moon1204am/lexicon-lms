d0091a871b3d4047d9c8efd25ef79aa37b3e5a76
EddMal <107246914+EddMal@users.noreply.github.com>
2024-01-03 14:02:12 +0100

    Under development added page.

---
+++ LexiconLMS/Client/Pages/CourseAdd.razor.cs
@@ -0,0 +1,36 @@
+﻿using LexiconLMS.Client.Services;
+using LexiconLMS.Shared.Dtos;
+using Microsoft.AspNetCore.Components;
+
+namespace LexiconLMS.Client.Pages
+{
+    public partial class CourseAdd
+    {
+
+        [Inject] 
+        public ILmsDataService LmsDataService { get; set; } = default!;
+       
+        
+        private CourseAddDto? CourseToAdd = new CourseAddDto();
+        
+
+        //protected override async Task OnInitializedAsync()
+        //{
+
+        //}
+
+        public async Task CreateCourseAsync()
+        {
+            if (CourseToAdd == null)
+            {
+
+            }
+            else
+            {
+                await LmsDataService.GetAsync<List<CourseDto>>($"api/courses/{CourseToAdd}");
+            }
+
+        }
+
+    }
+}
