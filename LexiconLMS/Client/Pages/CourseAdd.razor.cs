fcaac8704b7ce05479df8f11415c72460340bca1
Rabben82 <89846052+Rabben82@users.noreply.github.com>
2024-01-03 16:43:40 +0100

    Add New Course Works

--- LexiconLMS/Client/Pages/CourseAdd.razor.cs
+++ LexiconLMS/Client/Pages/CourseAdd.razor.cs
@@ -1,35 +1,27 @@
 ﻿using LexiconLMS.Client.Services;
 using LexiconLMS.Shared.Dtos;
 using Microsoft.AspNetCore.Components;
+using System.Net;
 
 namespace LexiconLMS.Client.Pages
 {
     public partial class CourseAdd
     {
-
         [Inject] 
         public ILmsDataService LmsDataService { get; set; } = default!;
-       
-        
         private CourseAddDto? CourseToAdd = new CourseAddDto();
-
-
-        protected override void OnInitialized()
-        {
-            base.OnInitialized();
-        }
 
         public async Task CreateCourseAsync()
         {
+       
             if (CourseToAdd == null)
             {
 
             }
             else
             {
-                await LmsDataService.GetAsync<List<CourseDto>>($"api/courses/{CourseToAdd}");
+                await LmsDataService.PostAsync<CourseAddDto>("api/courses/",CourseToAdd);
             }
-
         }
 
     }
