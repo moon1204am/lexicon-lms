c86a2f2147bb0113b7a9a0be2fef894688775ae1
Kasleets <141558322+Kasleets@users.noreply.github.com>
2024-01-03 12:34:34 +0100

    Fixed the code-behind file from the @code block. Continuing to fix the Error 500. User creation is succesful.

---
+++ LexiconLMS/Client/Pages/Admin.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using LexiconLMS.Shared.Dtos;
+using LexiconLMS.Client.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LexiconLMS.Client.Pages
+{
+    public partial class Admin : ComponentBase
+    {
+        // ILmsDataService is injected in the Admin.razor page.
+
+        protected UserDto newUser = new UserDto();
+        protected List<CourseDto> courses = new List<CourseDto>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            courses = await LmsDataService.GetAsync<List<CourseDto>>("api/courses");
+
+            if (courses.Count > 0)
+            {
+                newUser.CourseId = courses[0].Id;
+            }
+        }
+
+        protected async Task HandleValidSubmit()
+        {
+            await LmsDataService.PostAsync<UserDto, object>("api/users", newUser);
+            newUser = new UserDto();
+        }
+    }
+}
