eb42f91e57f0065a45ada316a4d3ac6f0445322b
Rabben82 <89846052+Rabben82@users.noreply.github.com>
2024-01-09 15:54:32 +0100

    Added delete button on courses in course.blazor

--- LexiconLMS/Client/Pages/Course.razor.cs
+++ LexiconLMS/Client/Pages/Course.razor.cs
@@ -9,30 +9,24 @@ namespace LexiconLMS.Client.Pages
 {
     public partial class Course
     {
-        //private ClaimsPrincipal _user;
-
         [Inject]
         public ILmsDataService LmsDataService { get; set; } = default!;
-        //[Inject]
-        //private IUserManager UserManager { }
-        //[Inject]
-        //private UserManager<IdentityUser> _userManager { get; set; } = default!;
-        //[Inject]
-        //AuthenticationStateProvider ASProvider { get; set; } = default!;
+        [Inject]
+        private NavigationManager NavigationManager { get; set; } = default!;
 
         [Parameter]
         public Guid CourseId { get; set; }
         public CourseDto? CourseDto { get; set; }
-        public bool IsOpen { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-          
             CourseDto = await LmsDataService.GetAsync<CourseDto>($"api/courses/{CourseId}");
-            //var authstate = await ASProvider.GetAuthenticationStateAsync();
-            //_user = authstate.User;
-
         }
-    
+        
+        public async Task DeleteCourseAsync()
+        {
+            await LmsDataService.DeleteAsync<Guid>($"api/courses/{CourseId}");
+            NavigationManager.NavigateTo("/courses");
+        }
     }
 }
